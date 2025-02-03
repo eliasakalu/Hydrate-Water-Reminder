543b794303f20ac0ad206431c216179eacf2ca73
Elias Akalu <143254520+sdhbdhf@users.noreply.github.com>
2025-02-03 17:43:03 +0300

    Update WaterReminder.js

--- WaterReminder.js
+++ WaterReminder.js
@@ -1,8 +1,8 @@
-var user = 9;
-const litters = [2, 3, 4, 5, 6, 7, user];
+let user = 9;
+const liters = [2, 3, 4, 5, 6, 7, user];
 
 function openPopUp() {
-    document.querySelector("#message2").innerHTML = `Hello! You Set ${user} Litter Water! Don't forget to do it today`;
+    document.querySelector("#message2").innerHTML = `Hello! You Set ${user} Liters of Water! Don't forget to do it today`;
     document.querySelector('.value8').addEventListener("click", () => {
         const message = document.querySelector("#sent");
         message.id = "sent2";
@@ -14,7 +14,7 @@ function openPopUp() {
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-    function updateclock() {
+    function updateClock() {
         const now = new Date();
         const hours = now.getHours();
         const currenttime = now.toLocaleString();
@@ -26,8 +26,8 @@ document.addEventListener("DOMContentLoaded", function() {
             document.querySelector("#datetime").textContent = "Good Evening";
         }
     }
-    setInterval(updateclock, 1000);
-    updateclock();
+    setInterval(updateClock, 1000);
+    updateClock();
 
     const taskContainer = document.querySelector("#task");
     const taskAdderButton = document.querySelector("#taskadder");
@@ -42,11 +42,8 @@ document.addEventListener("DOMContentLoaded", function() {
         taskContainer.style.display = "block";
     });
 
-    const addTaskButton = document.querySelector("#textsbutton");
-    addTaskButton.addEventListener("click", () => {
+    document.querySelector("#textsbutton").addEventListener("click", () => {
         const taskText = document.querySelector("#texts").value.trim();
-        const taskList = document.querySelector("#task ul");
-
         if (taskText) {
             const newTask = document.createElement("li");
             newTask.textContent = taskText;
@@ -54,59 +51,27 @@ document.addEventListener("DOMContentLoaded", function() {
             removeButton.textContent = "Remove";
             removeButton.classList.add("remove-task");
             newTask.appendChild(removeButton);
-            taskList.appendChild(newTask);
+            document.querySelector("#task ul").appendChild(newTask);
             document.querySelector("#texts").value = "";
 
-            removeButton.addEventListener("click", (e) => {
-                const taskItem = e.target.closest("li");
-                taskItem.remove();
+            removeButton.addEventListener("click", () => {
+                newTask.remove();
             });
 
-            taskContainer.style.display = "none";  // Close task box after adding task
+            taskContainer.style.display = "none";
         } else {
             alert("Please enter a task!");
         }
     });
 
-    const closeButton = document.querySelector("#exit2");
-    closeButton.addEventListener("click", () => {
-        taskContainer.style.display = "none";  // Hide task container when close button is clicked
+    document.querySelector("#exit2").addEventListener("click", () => {
+        taskContainer.style.display = "none";
     });
 
-    const setMyLiterButton = document.querySelector(".value1");
-    setMyLiterButton.addEventListener("click", () => {
-        user = 2;
-        openPopUp();
-    });
-    const setMyLiterButton2 = document.querySelector(".value2");
-    setMyLiterButton2.addEventListener("click", () => {
-        user = 3;
-        openPopUp();
-    });
-    const setMyLiterButton3 = document.querySelector(".value3");
-    setMyLiterButton3.addEventListener("click", () => {
-        user = 4;
-        openPopUp();
-    });
-    const setMyLiterButton4 = document.querySelector(".value4");
-    setMyLiterButton4.addEventListener("click", () => {
-        user = 5;
-        openPopUp();
-    });
-    const setMyLiterButton5 = document.querySelector(".value5");
-    setMyLiterButton5.addEventListener("click", () => {
-        user = 6;
-        openPopUp();
-    });
-    const setMyLiterButton6 = document.querySelector(".value6");
-    setMyLiterButton6.addEventListener("click", () => {
-        user = 7;
-        openPopUp();
-    });
-    const setMyLiterButton7 = document.querySelector(".value7");
-    setMyLiterButton7.addEventListener("click", () => {
-        let response = prompt("How Much Litters Do you want to set?");
-        user = response;
-        openPopUp();
+    document.querySelectorAll('.set-liter').forEach(button => {
+        button.addEventListener("click", () => {
+            user = button.dataset.liter || prompt("How Many Liters Do you want to set?");
+            openPopUp();
+        });
     });
 });
